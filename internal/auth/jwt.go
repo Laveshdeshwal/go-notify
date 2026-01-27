a6bac593145181d9734e08c19bc06d2be00d62c8
lokeshdeshwal <lokesh.deshwal@kissht.com>
2026-01-27 21:54:15 +0530

    JWT feature added

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,1 +1,62 @@
-package jwt
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var (
+	// todo : In production: load from env / secret manager
+	jwtSecret = []byte("super-secret-key")
+)
+
+// CustomClaims defines what we store inside JWT
+type CustomClaims struct {
+	Username string `json:"username"`
+	Role     string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+// CreateToken generates a signed JWT
+func CreateToken(username, role string) (string, error) {
+	claims := CustomClaims{
+		Username: username,
+		Role:     role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "go-notify",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(15 * time.Minute)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+// ParseAndValidateToken validates JWT and returns claims
+func ParseAndValidateToken(tokenString string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&CustomClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			// Protect against alg spoofing
+			if token.Method != jwt.SigningMethodHS256 {
+				return nil, errors.New("unexpected signing method")
+			}
+			return jwtSecret, nil
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*CustomClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
+}
